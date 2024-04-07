ace10ab3207d26003f84d8ac9c1c313646e83416
Sai Karthik <venkatasaikarthikeya@gmail.com>
2024-04-07 16:57:04 -0700

    authentication in place

--- webapp/views.py
+++ webapp/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render, redirect
 from .forms import CreateUserForm, LoginForm
 from django.contrib.auth.models import auth
 from django.contrib.auth import authenticate
-
+from django.contrib.auth.decorators import login_required
 
 # Home page
 def home(request):
@@ -16,7 +16,7 @@ def register(request):
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-            return redirect('login')
+            return redirect("login")
     context = {'form': form}
     return render(request, 'webapp/register.html', context=context)
 
@@ -32,6 +32,18 @@ def login(request):
             user = authenticate(request, username=username, password=password)
             if user is not None:
                 auth.login(request, user)
-                # return redirect('')
+                return redirect("dashboard")
     context = {'form': form}
     return render(request, 'webapp/login.html', context=context)
+
+
+# User logout
+def logout(request):
+    auth.logout(request)
+    return redirect("login")
+    
+
+# Dashboard
+@login_required(login_url='login')
+def dashboard(request):
+    return render(request, 'webapp/dashboard.html')
