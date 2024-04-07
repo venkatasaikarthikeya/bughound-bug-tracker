e3f462bcd0f98e08f497240b6a5a970e5cb9cdbb
Sai Karthik <venkatasaikarthikeya@gmail.com>
2024-04-06 19:01:38 -0700

    register completed

--- webapp/views.py
+++ webapp/views.py
@@ -1,12 +1,16 @@
-from django.shortcuts import render
-
-from django.http import HttpResponse
-
-# Create your views here.
+from django.shortcuts import render, redirect
+from .forms import CreateUserForm, LoginForm
 
 def home(request):
     return render(request, 'webapp/index.html')
-    #return HttpResponse("Hello World")
 
 
-
+def register(request):
+    form = CreateUserForm()
+    if request.method == 'POST':
+        form = CreateUserForm(request.POST)
+        if form.is_valid():
+            form.save()
+            # return redirect('webapp/login.html')
+    context = {'form': form}
+    return render(request, 'webapp/register.html', context=context)
